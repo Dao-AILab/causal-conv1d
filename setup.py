8aaaeda0560f5c8681087ec14feb13c5ee6f0db9
Tri Dao <tridpq@gmail.com>
2025-08-19 16:59:50 -0400

    Remove sm_53, add sm_120
    sm_53 is no longer supported in nvcc 13.0

--- setup.py
+++ setup.py
@@ -174,8 +174,6 @@ if not SKIP_CUDA_BUILD:
                 )
 
         cc_flag.append("-gencode")
-        cc_flag.append("arch=compute_53,code=sm_53")
-        cc_flag.append("-gencode")
         cc_flag.append("arch=compute_62,code=sm_62")
         cc_flag.append("-gencode")
         cc_flag.append("arch=compute_70,code=sm_70")
@@ -191,6 +189,8 @@ if not SKIP_CUDA_BUILD:
         if bare_metal_version >= Version("12.8"):
             cc_flag.append("-gencode")
             cc_flag.append("arch=compute_100,code=sm_100")
+            cc_flag.append("-gencode")
+            cc_flag.append("arch=compute_120,code=sm_120")
 
     # HACK: The compiler flag -D_GLIBCXX_USE_CXX11_ABI is set to be the same as
     # torch._C._GLIBCXX_USE_CXX11_ABI
